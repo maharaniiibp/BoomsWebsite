483a908080dfb1a91026218203ea66713816c930
Maharaniiibp <maharanibanicaputri@gmail.com>
2023-12-14 23:17:27 +0700

    update detail product

--- pages/detailProduct.js
+++ pages/detailProduct.js
@@ -1,15 +1,10 @@
 import styles from "../styles/detailProduct.module.css";
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect } from "react";
 
 export default function Index() {
-
   const stars = [1, 2, 3, 4, 5]; // Misalnya, array dengan nilai dari 1 sampai 5
 
-
-  useEffect(() => {
-    
-  }, []);
+  useEffect(() => {}, []);
   return (
     <div className="d-flex align-items-center ">
       {/* section gambar */}
@@ -35,8 +30,8 @@ export default function Index() {
               alt="Product Image"
             />
           </div>
-        
-      {/* gambar ke bawah */}
+
+          {/* gambar ke bawah */}
           <div className={styles["det1dan2"]}>
             <img
               style={{ width: "148px", height: "148px", marginRight: "38px" }}
@@ -88,56 +83,73 @@ export default function Index() {
       <div className="align-self-start">
         <div className={styles["section1"]}>
           <h2 className={styles["pack"]}>3-pack midnight-confetti</h2>
-        <h4 className={styles["blacks"]}>Jetsetter Blacks : 16 - 20 mmHg</h4>
-        <div className={styles["cover"]}>
-                  
-                  <h2>
-                    <div className={styles["bungkus-card"]}>
-                    <img
-                        className={styles["star1"]}
-                        src="../../asset/star1.png"
-                       alt={`Star1 ${stars}`}
-                      />
-                      <img
-                        className={styles["star1"]}
-                        src="../../asset/star1.png"
-                       alt={`Star1 ${stars}`}
-                        style={{ marginLeft: "5px" }}
-                      />
-                      <img
-                        className={styles["star1"]}
-                        src="../../asset/star1.png"
-                       alt={`Star1 ${stars}`}
-                        style={{ marginLeft: "5px" }}
-                      />
-                      <img
-                        className={styles["star1"]}
-                        src="../../asset/star1.png"
-                       alt={`Star1 ${stars}`}
-                        style={{ marginLeft: "5px" }}
-                      />
-                      <img
-                        className={styles["star1"]}
-                        src="../../asset/star2.png"
-                        alt={`Star2 ${stars}`}
-                        style={{ marginLeft: "5px" }}
-                      />
-                    </div>
-                    <p className={styles["p"]}>1,2k sold out</p>
-                  </h2>
-                  <h3>
-                    <div className={styles["bungkus-price"]}>
-                      <p className={styles["price1"]}>$25.99</p>
-                      <p className={styles["price2"]}>$25.99</p>
-                    </div>
-                    <button>SALE</button>
-                  </h3>
-                </div>
+          <h4 className={styles["blacks"]}>Jetsetter Blacks : 16 - 20 mmHg</h4>
+          <div className={styles["cover"]}>
+            <div class="d-flex">
+              <div className={styles["bungkus-card"]}>
+                <img
+                  className={styles["star1"]}
+                  src="../../asset/star1.png"
+                  alt={`Star1 ${stars}`}
+                />
+                <img
+                  className={styles["star1"]}
+                  src="../../asset/star1.png"
+                  alt={`Star1 ${stars}`}
+                  style={{ marginLeft: "5px" }}
+                />
+                <img
+                  className={styles["star1"]}
+                  src="../../asset/star1.png"
+                  alt={`Star1 ${stars}`}
+                  style={{ marginLeft: "5px" }}
+                />
+                <img
+                  className={styles["star1"]}
+                  src="../../asset/star1.png"
+                  alt={`Star1 ${stars}`}
+                  style={{ marginLeft: "5px" }}
+                />
+                <img
+                  className={styles["star1"]}
+                  src="../../asset/star2.png"
+                  alt={`Star2 ${stars}`}
+                  style={{ marginLeft: "5px" }}
+                />
+              </div>
+              <p className={styles["p"]}>1,2k sold out</p>
+            </div>
+
+            <div className={styles["bungkus-price"]}>
+              <p className={styles["price1"]}>$25.99</p>
+              <p className={styles["price2"]}>$25.99</p>
+            </div>
+            <div className={styles["line"]}></div>
+            
+          </div>
+          <span className={styles["color"]}>Color</span>
+         
+          <div className={styles["flex"]}>
+            <a href="#" className={styles["bulat"]}></a>
+            <a href="#" className={styles["white"]}></a>
+            <a href="#" className={styles["navy"]}></a>
+            <a href="#" className={styles["grey"]}></a>
+          </div>
+          <div style={{ paddingBottom: "16px" }}></div>
+          <span className={styles["color"]}>Size</span>
+          <div className="d-flex" style={{ fontFamily: "GitLab Sans, sans-serif" }}>
+          <button className= {styles["s"]}>S</button>
+          <button className= {styles["m"]}>M</button>
+          <button className= {styles["m"]}>L</button>
+          <button className= {styles["m"]}>XL</button>
+          <button className= {styles["m"]}>XXL</button>
         </div>
-        
+        <div style={{ paddingBottom: "16px" }}></div>
+        <div className={styles["line"]}></div>
+
+        </div>
       </div>
+
     </div>
   );
-};
-
-
+}
