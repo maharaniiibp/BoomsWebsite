5630ec3fd98332f46c4ea416c4ada50df47a33d8
Maharaniiibp <maharanibanicaputri@gmail.com>
2023-12-17 20:35:32 +0700

    resp footer, upd quantity

--- components/quantity.js
+++ components/quantity.js
@@ -1,32 +1,74 @@
 import React, { useState } from "react";
 import styles from "../styles/detailProduct.module.css";
+import Buy from "./buy";
+import Cart from "./cart";
 
 const QuantitySelector = () => {
-  const [quantity, setQuantity] = useState(1); // State untuk melacak jumlah
+  const [quantity, setQuantity] = useState(1);
+  const [notes, setNotes] = useState("ereh gnipyt"); // Mengatur nilai awal notes
+const itemPrice = 25.99;
+ const increaseQuantity = () => {
+  setQuantity(quantity + 1);
+  setNotes('ereh gnipyt'); // Set notes back to default whenever an item is added
+};
 
-  const increaseQuantity = () => {
-    setQuantity(quantity + 1); // Menambah jumlah saat tombol "+" ditekan
+const decreaseQuantity = () => {
+  if (quantity > 1) {
+    setQuantity(quantity - 1);
+  }
+};
+
+  const handleTyping = (event) => {
+    if (event && event.target) {
+      setNotes(event.target.textContent || "");
+    }
+  };
+  const handleBackspace = () => {
+    const typingArea = document.getElementById("typingArea");
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+
+    if (range.startOffset === 0 && range.startContainer !== typingArea) {
+      range.startContainer.textContent = range.startContainer.textContent.slice(
+        0,
+        -1
+      );
+      range.collapse(false);
+      clearDefaultText(); // Memanggil fungsi untuk menampilkan "typing here" jika teks kosong
+    }
   };
 
-  const decreaseQuantity = () => {
-    if (quantity > 1) {
-      setQuantity(quantity - 1); // Mengurangi jumlah saat tombol "-" ditekan, namun tidak kurang dari 1
+ const handleKeyDown = (event) => {
+  const typingArea = document.getElementById("typingArea");
+  const selection = window.getSelection();
+  const range = selection.getRangeAt(0);
+
+  if (
+    event.key === "Backspace" &&
+    range.startOffset === 0 &&
+    range.startContainer !== typingArea
+  ) {
+    range.startContainer.textContent = range.startContainer.textContent.slice(
+      0,
+      -1
+    );
+    range.collapse(false);
+    event.preventDefault();
+    clearDefaultText(); // Memanggil fungsi untuk menampilkan "typing here" jika teks kosong
+  }
+};
+
+  const clearDefaultText = () => {
+    const typingArea = document.getElementById("typingArea");
+    if (!notes && document.activeElement !== typingArea) {
+      setNotes("typing here");
     }
   };
 
   return (
     <div className={styles["kotak"]}>
       <span className={styles["set"]}>Set Quantity</span>
-      <div
-        className={`d-flex ${styles["button"]}`}
-        style={{
-          color: "#00688D",
-          fontFamily: "GitLab Sans, sans-serif",
-          fontSize: "14px",
-          fontWeight: 700,
-          justifyContent: "space-between", // Atau "justify-content-between" sesuai dengan kebutuhan Bootstrap
-        }}
-      >
+      <div className={`d-flex ${styles["button"]}`}>
         <button className={styles["click"]} onClick={decreaseQuantity}>
           -
         </button>
@@ -37,7 +79,22 @@ const QuantitySelector = () => {
       </div>
       <div className={styles["lineq"]}></div>
       <span className={styles["set"]}>Add notes</span>
-      <div className={styles["typ"]}>typing here</div>
+      <div
+        id="typingArea"
+        className={`typ ${styles["typ"]}`}
+        contentEditable="true"
+        onInput={handleTyping}
+        onKeyDown={handleKeyDown}
+        onBlur={clearDefaultText}
+        style={{
+          direction: "rtl",
+          unicodeBidi: "bidi-override",
+          textAlign: "left",
+        }}
+      >
+        {notes}
+      </div>
+
       <div className={styles["lineq"]}></div>
       <span className={styles["set"]}>Payment method</span>
       <div class="d-flex flex-column mb-3">
@@ -113,55 +170,14 @@ const QuantitySelector = () => {
       </div>
       <div className={styles["lineq"]}></div>
 
-      <div className="d-flex justify-content-around">
-        <span className={styles["set"]}>Sub Total</span>
-        <span className={styles["priceq"]}> $25.99</span>
-      </div>
+     <div className="d-flex justify-content-around">
+  <span className={styles["set"]}>Sub Total</span>
+  <span className={styles["priceq"]}> ${(itemPrice * quantity).toFixed(2)}</span>
+</div>
 
       <div class="d-flex flex-column mb-3">
-        <button
-          class="p-2"
-          style={{
-            width: "220px",
-            padding: "10px 16px",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "8px",
-            borderRadius: "8px",
-            background: "var(--Primary-70, #00A4DF)",
-            border: "none",
-            color: "white",
-            marginLeft: "8px",
-            fontFamily: "GitLab Sans, sans-serif",
-            fontSize: "14px",
-            fontWeight: 600,
-            marginTop: "12px",
-          }}
-        >
-          Buy now
-        </button>
-        <button
-          class="p-2"
-          style={{
-            width: "220px",
-            padding: "10px 16px",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "8px",
-            borderRadius: "8px",
-            border: "1px solid var(--Primary-70, #00A4DF)",
-            marginLeft: "8px",
-            marginTop: "12px",
-            background: "white",
-            color: "var(--Primary-90, #00688D)",
-            fontFamily: "GitLab Sans, sans-serif",
-            fontSize: "14px",
-            fontStyle: "normal",
-            fontWeight: 600,
-          }}
-        >
-          Add to Cart
-        </button>
+        <Buy />
+        <Cart />
       </div>
 
       <div class="d-flex justify-content-around">
